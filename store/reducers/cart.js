72ebdda9555b3e654547add50cfb789b8fbca45f
psokolnikov <sokolnikov90@gmail.com>
2021-11-29 07:45:40 +0300

    Add REMOVE_FROM_CART reducer

--- store/reducers/cart.js
+++ store/reducers/cart.js
@@ -1,5 +1,5 @@
 import CartItem from '../../models/cart-item';
-import { ADD_TO_CART } from '../actions/cart';
+import { ADD_TO_CART, REMOVE_FROM_CART } from '../actions/cart';
 
 const initialState = {
 	items: {},
@@ -30,6 +30,32 @@ export default (state = initialState, action) => {
 				...state,
 				items: { ...state.items, [addedProduct.id]: updatedOrNewCartItem },
 				totalAmount: state.totalAmount + prodPrice,
+			};
+
+		case REMOVE_FROM_CART:
+			const selectedCartItem = state.items[action.productId];
+
+			if (!selectedCartItem) return state;
+
+			let updatedCartItems;
+			if (selectedCartItem.quantity > 1) {
+				const updatedCartItem = new CartItem(
+					selectedCartItem.quantity - 1,
+					selectedCartItem.productPrice,
+					selectedCartItem.productTitle,
+					selectedCartItem.sum - selectedCartItem.productPrice
+				);
+	
+				updatedCartItems = {...state.items, [action.productId]: updatedCartItem };
+			} else {
+				updatedCartItems = {...state.items};
+				delete updatedCartItems[action.productId];
+			}
+
+			return {
+				...state,
+				items: updatedCartItems,
+				totalAmount: state.totalAmount - selectedCartItem.productPrice
 			};
 
 		default:
