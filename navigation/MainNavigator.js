c5666dfa041f0c03dad0bfd77f2e2fee23426fb5
psokolnikov <sokolnikov90@gmail.com>
2021-12-05 17:44:32 +0300

    Rename ShopNavigator => MainNavigator

--- navigation/MainNavigator.js
+++ navigation/MainNavigator.js
@@ -104,7 +104,6 @@ const ShopNavigator = createDrawerNavigator(
 					<DrawerItems {...props}/>
 					<Button title='Logout' color={Colors.primary} onPress={() => {
 						dispatch(authActions.logout());
-						// props.navigation.navigate('Auth');
 					}} />
 				</SafeAreaView>
 			</View>;
--- navigation/NavigationContainer.js
+++ navigation/NavigationContainer.js
@@ -2,7 +2,7 @@ import React, { useEffect, useRef } from 'react';
 import { useSelector } from 'react-redux';
 import { NavigationActions } from 'react-navigation'
 
-import ShopNavigator from './ShopNavigator';
+import MainNavigator from './MainNavigator';
 
 const NavigationContainer = props => {
     const navRef = useRef();
@@ -16,7 +16,7 @@ const NavigationContainer = props => {
         }
     }, [isAuth])
 
-    return <ShopNavigator ref={navRef} />
+    return <MainNavigator ref={navRef} />
 };
 
 export default NavigationContainer;
